154cc05301518c8b5ff3516ad4365d6d9f182a35
vyapakmishra1250 <vyapakdikmishra1250@gmail.com>
2021-06-23 20:33:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// react router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//components
+import SignIn from './Components/SignIn';
+import Home from './Components/Home';
+
+const App = () => {
+  const [viewOtpForm, setViewOtpForm] = useState(false);
+  const [user, setUser] = useState(false);
+  const firebaseConfig = {
+    apiKey: "AIzaSyDsC1E0bOa0hyjT_zBK8sU0YnxE1xw2NMA",
+    authDomain: "fir-auth-otp-1008d.firebaseapp.com",
+    projectId: "fir-auth-otp-1008d",
+    storageBucket: "fir-auth-otp-1008d.appspot.com",
+    messagingSenderId: "180895592045",
+    appId: "1:180895592045:web:c79cbb85b040cd8077d964",
+    measurementId: "G-JX4327CDP3",
+  };
+
+  useEffect(() => {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: function (response) {
+          console.log("Captcha Resolved");
+          this.onSignInSubmit();
+        },
+        defaultCountry: "IN",
+      }
+    );
+  }, []);
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
+
+  const auth = firebase.auth();
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUser(user);
+    }
+  });
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+
+    let phone_number = "+91" + e.target.phone.value;
+    const appVerifier = window.recaptchaVerifier;
+
+    auth
+      .signInWithPhoneNumber(phone_number, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        console.log("otp sent");
+        setViewOtpForm(true);
+        window.confirmationResult = confirmationResult;
+        // ...
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        // ...
+        alert(error.message);
+      });
+  };
+
+  const otpSubmit = (e) => {
+    e.preventDefault();
+
+    let opt_number = e.target.otp_value.value;
+
+    window.confirmationResult
+      .confirm(opt_number)
+      .then((confirmationResult) => {
+        console.log(confirmationResult);
+        console.log("success");
+        window.open("/", "_self");
+      })
+      .catch((error) => {
+        // User couldn't sign in (bad verification code?)
+        alert(error.message);
+      });
+  };
+
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        window.open("/signin", "_self");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="recaptcha-container"></div>
+      <Switch>
+        <Route path="/" exact>
+          <Home signOut={signOut} user={user} />
+        </Route>
+        <Route path="/signin" exact>
+          <SignIn
+            loginSubmit={loginSubmit}
+            otpSubmit={otpSubmit}
+            viewOtpForm={viewOtpForm}
+          />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
